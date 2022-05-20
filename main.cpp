d7a419fc3e295fe80d4d8dbac814b582d0cc30f8
Frans <97081479+Fransssss@users.noreply.github.com>
2022-05-20 03:31:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-// Title:
-// Purpose:
+// Title: Person Class
+// Purpose: Create basic class that will be used for a bigger program (to be updated)
 // Author: Fransiskus Agapa
 // Have fun - Enjoy the process - Practices make improvement
 
@@ -11,7 +11,7 @@ using std::cin;
 int main()
 {
     cout << endl;
-    Person perO("Andy", "1234567899", 4);
+    Person perO("Andy", "1234567899", 4);                                               // create object type Person class and set value 
     Person perT("Bandy", "1122334455", 6);
     Person perTh("Cindy", "9876543211", 8);
 
@@ -24,10 +24,6 @@ int main()
     cout << endl;
     cout << perTh.ToString() << endl;
     cout << perTh.ToJson() << endl;
-
-
-
-
   
  return 0; 
 }
